dc7437153773bc3d7380d7524326ba7472328598
Musse <86mussai@skola.boras.se>
2025-02-10 13:20:29 +0100

    skapat sass file

--- src/Navbar/Nav.tsx
+++ src/Navbar/Nav.tsx
@@ -3,8 +3,17 @@ import logo from "../images/logo.png";
 const Nav = () => {
   return (
     <div>
+      <section>
         <img src={logo} width={50} height={50} alt="logo" />
-      <h1>Game-Home</h1>
+        <h1>Game-Home</h1>
+      </section>
+      <section>
+        <ul>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About Us</li>
+        </ul>
+      </section>
     </div>
   );
 };
