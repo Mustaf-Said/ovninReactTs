b97f14a65e980629f03869535df227b177eb23ff
Musse <86mussai@skola.boras.se>
2025-02-10 13:13:47 +0100

    övning med React + Ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Nav from "./Navbar/Nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <div>Hello World</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar/Nav.tsx
@@ -0,0 +1,12 @@
+import logo from "../images/logo.png";
+
+const Nav = () => {
+  return (
+    <div>
+        <img src={logo} width={50} height={50} alt="logo" />
+      <h1>Game-Home</h1>
+    </div>
+  );
+};
+
+export default Nav;
