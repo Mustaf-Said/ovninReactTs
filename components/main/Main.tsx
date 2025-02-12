7ee254b029a088f88f97c3aa280b049ac26f0232
Musse <86mussai@skola.boras.se>
2025-02-12 07:01:26 +0100

    update with fruits data

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,8 +1,29 @@
 import Blog from "../blog/Blog";
+import "../../scss/blog.scss";
+import data from "../blog/fruits.json";
+
+interface getFruits {
+  category: string;
+  price: string;
+  stocked: boolean;
+  name: string;
+}
 function Main() {
   return (
     <div>
-      <Blog />
+      <div>
+        <Blog />
+      </div>
+      <div className="fruits">
+        {data.map((items: getFruits) => (
+          <div key={items.name}>
+            <h3>{items.category}</h3>
+            <h6>{items.name}</h6>
+            <p>{items.stocked ? "In stock" : "Out of stock"}</p>
+            <p>{items.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
