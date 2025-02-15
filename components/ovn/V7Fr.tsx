67e72d565a29df862710ca066aa977b10d1851b1
Musse <86mussai@skola.boras.se>
2025-02-15 05:32:28 +0100

    Update ts & sass

--- components/ovn/V7Fr.tsx
+++ components/ovn/V7Fr.tsx
@@ -1,5 +1,6 @@
-import { MouseEventHandler, useState } from "react";
 import "../../scss/test.scss";
+/* import { MouseEventHandler, useState } from "react";
+
 
 interface V7FrProps {
   name: string;
@@ -43,3 +44,52 @@ function V7Fr2() {
 }
 
 export default V7Fr2;
+ */
+
+//-------------------------------------------------------------
+
+import React, { useState, FormEvent } from "react";
+
+const Test: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<string>("");
+  const [submittedName, setSubmittedName] = useState<string>("");
+  const [submittedAge, setSubmittedAge] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setSubmittedName(name);
+    setSubmittedAge(age);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="form">
+        <label htmlFor="name" className="btn1">
+          Write your name here:
+        </label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="age"> Write your age here:</label>
+        <input
+          type="number"
+          id="age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          className="btn2"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <p>{submittedName}</p>
+        <span>{submittedAge}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Test;
